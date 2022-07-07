3b6f2485a3e20eb6f027dae148c7ac5cf97211a3
Eshaan Bansal <101061994+eshaan-deepsource@users.noreply.github.com>
2022-07-07 14:53:50 +0530

    Update test_code.py

--- tests/test_code.py
+++ tests/test_code.py
@@ -8,8 +8,5 @@ def test_random_number_generator():
 
 
 class Tests(unittest.TestCase):
-    def my_test(self, arg1, arg2):
-        self.assertEquals(arg1, arg2)
+    pass
 
-    def my_test2(self, arg1, arg2):
-        self.assertEquals(arg1, arg2)
