d1b48e49ca63c07d80442a523c7b8529e1ef55cf
Eshaan Bansal <101061994+eshaan-deepsource@users.noreply.github.com>
2022-07-12 20:22:59 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,4 @@
 (*FIRST,) = [1, 2, 3]  # documenting code no fun
 (*FIRST,) = [1, 2, 3]  # documenting code no fun
 *FIRST, a, b = [1, 2, 3]  # documenting code no fun
+a, b = [1, 2]  # documenting code no fun
